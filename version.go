a3b8a433999e78b66f0aa15d722761cdbf309704
alexstocks <dragoncharlie@foxmail.com>
2016-10-22 23:29:26 +0800

    add remark

--- version.go
+++ version.go
@@ -19,7 +19,8 @@ const (
 
 /*
  几个可能的优化点：
- 1 session.go:Session中的rQ & wQ 均是channel实现，其本质是bounded queue，所以可能导致读写阻塞，如果想让write更快一点，可以使用list作为wQ的二级队列，以存放优先级比较高的且不能丢弃(在wQ full的情况下)的pkg；
+ 1 session.go:Session中的rQ & wQ 均是channel实现，其本质是bounded queue，所以可能导致读写阻塞，
+   如果想让write更快一点，可以使用list作为wQ的二级队列(或者称之为优先级队列)，以存放优先级比较高的且不能丢弃(在wQ full的情况下)的pkg；
  2 codec.go:(EventListener)OnMessage其实可以一次处理多个session.go:Session{rQ}的多个包，如：
    const MAX = 32
    for {
