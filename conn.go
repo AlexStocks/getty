f36fc366fd12375b674d8da6cd11c58a120636cc
AlexStocks <alexstocks@foxmail.com>
2018-03-18 20:00:44 +0800

    set peer address for udp client

--- conn.go
+++ conn.go
@@ -418,7 +418,7 @@ func (u *gettyUDPConn) Write(udpCtx interface{}) (int, error) {
 	if buf, ok = ctx.Pkg.([]byte); !ok {
 		return 0, fmt.Errorf("illegal @udpCtx.Pkg{%#v} type", udpCtx)
 	}
-	if u.ss.EndPointType() == UDP_CLIENT {
+	if u.ss.EndPointType() == UDP_ENDPOINT {
 		peerAddr = ctx.PeerAddr
 		if peerAddr == nil {
 			return 0, ErrNullPeerAddr
