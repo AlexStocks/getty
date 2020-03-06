db214bfc6001b13b2341ced951d1e145a463db1c
watermelo <80680489@qq.com>
2020-03-06 11:13:53 +0800

    test udpconn no be assigned localAddr

--- transport/client_test.go
+++ transport/client_test.go
@@ -131,6 +131,19 @@ func TestTCPClient(t *testing.T) {
 	assert.True(t, clt.IsClosed())
 }
 
+func TestUDPIpNotBeAssigned(t *testing.T) {
+	ip := net.ParseIP("127.0.0.1")
+	srcAddr := &net.UDPAddr{IP: net.IPv4zero, Port: 0}
+	dstAddr := &net.UDPAddr{IP: ip, Port: 9999}
+	conn, err := net.DialUDP("udp", srcAddr, dstAddr)
+	if err != nil {
+		t.Logf("======udp test error info: %v", err)
+	} else {
+		t.Logf("======udp test assign info: %v, %v", conn.LocalAddr(), conn.RemoteAddr())
+	}
+	defer conn.Close()
+}
+
 func TestUDPClient(t *testing.T) {
 	var (
 		err  error
