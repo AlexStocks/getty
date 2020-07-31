f2a7d3d5de8471e73c70abd95a18de4ba29b2b2e
watermelo <80680489@qq.com>
2020-07-31 17:09:48 +0800

    Add: add apache license

--- examples/echo/wss-echo/server/app/config.go
+++ examples/echo/wss-echo/server/app/config.go
@@ -1,11 +1,19 @@
-/******************************************************
-# DESC       : env var & configure
-# MAINTAINER : Alex Stocks
-# LICENCE    : Apache License 2.0
-# EMAIL      : alexstocks@foxmail.com
-# MOD        : 2016-09-06 16:53
-# FILE       : config.go
-******************************************************/
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package main
 
--- transport/session.go
+++ transport/session.go
@@ -1,11 +1,19 @@
-/******************************************************
-# DESC       : session
-# MAINTAINER : Alex Stocks
-# LICENCE    : Apache License 2.0
-# EMAIL      : alexstocks@foxmail.com
-# MOD        : 2016-08-17 11:21
-# FILE       : session.go
-******************************************************/
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package getty
 
@@ -800,17 +808,17 @@ func (s *session) handleTCPPackage() error {
 // get package from udp packet
 func (s *session) handleUDPPackage() error {
 	var (
-		ok       bool
-		err      error
-		netError net.Error
-		conn     *gettyUDPConn
-		bufLen   int
+		ok        bool
+		err       error
+		netError  net.Error
+		conn      *gettyUDPConn
+		bufLen    int
 		maxBufLen int
-		bufp     *[]byte
-		buf      []byte
-		addr     *net.UDPAddr
-		pkgLen   int
-		pkg      interface{}
+		bufp      *[]byte
+		buf       []byte
+		addr      *net.UDPAddr
+		pkgLen    int
+		pkg       interface{}
 	)
 
 	conn = s.Connection.(*gettyUDPConn)
